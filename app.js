e348c769b30a5e9ac78a6876838e0cbfdb316db9
Silas Morales <silas.mora22@gmail.com>
2023-01-04 07:57:03 -0800

    Exercise Complete

--- app.js
+++ app.js
@@ -7,13 +7,54 @@ console.log(
 console.log("EXERCISE 1:\n==========\n");
 
 // YOUR CODE HERE
+let a = 20;
+let b = 4;
+
+let add = a + b;
+let minus = a - b;
+let multiply = a * b;
+let dividing = a / b;
+
+console.log(add)
+console.log(minus)
+console.log(multiply)
+console.log(dividing)
 
 // Exercise 2
 console.log("EXERCISE 2:\n==========\n");
 
 // YOUR CODE HERE
 
+let num = 11;
+let str = "11";
+let str2 = "eleven";
+let isPresent = true;
+let firstName = "Frodo";
+let lastName = "Baggins";
+
+console.log(num + str)
+console.log(num + str2)
+console.log(num + isPresent)
+console.log(firstName + num)
+console.log(isPresent + str)
+console.log(firstName + lastName)
+
 // Exercise 3
 console.log("EXERCISE 3:\n==========\n");
 
 // YOUR CODE HERE
+
+let val = 5;
+let str3 = "5";
+let str4 = "five";
+let isPresent2 = false;
+
+console.log(val==str3)
+console.log(val===str3)
+console.log(!isPresent2)
+console.log("eleven"==str4&&val>=str3)
+console.log(!isPresent2||isPresent2)
+console.log(0==false)
+console.log(0===false)
+console.log(0!=false)
+console.log(0!==false)
